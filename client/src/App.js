f8f4672464b38ce3879a52cc58f7535f35d5e381
ajaz.s <ajaz.s@zohocorp.com>
2021-06-08 22:56:40 +0530

    event tracking also added

--- client/src/App.js
+++ client/src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
+      this.listenToPaymentEvent();
       this.setState({ loaded : true });
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -38,6 +39,18 @@ class App extends Component {
       console.error(error);
     }
   };
+
+  listenToPaymentEvent = () => {
+    let self = this
+    this.itemManager.events.SupplyChainStep().on("data" , async function (event) {
+      if(event.returnValues._step == 1) {
+        let  itemObj = await self.itemManager.methods.items(event.returnValues._itemInde).call();
+        console.log(itemObj)
+        alert("Item " + itemObj._identifier + " was paid, deliver it now!");
+      }
+      console.log(event)     
+    })
+  }
 
 
   handleInputChange = (event) => {
@@ -53,11 +66,10 @@ class App extends Component {
   handleSubmit = async() => {
     const {cost , itemName} = this.state
     console.log(itemName, cost, this.itemManager , this.accounts);
-    let result = this.itemManager.methods.createItem(itemName , cost).send({from : this.accounts[0]}).then(function(result){
+    let result = await this.itemManager.methods.createItem(itemName , cost).send({from : this.accounts[0]})
+
     console.log(result);
     alert("Send "+cost+" Wei to "+result.events.SupplyChainStep.returnValues._address);
-    });
-
 
   }
 
